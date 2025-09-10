8b534d40c7edb1ae166058b834001b417dcdcfe9
juanbenitez2005 <jbenitezpallares@gmail.com>
2025-09-09 21:57:33 -0300

    Struct avl y funciones de comparacion

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,6 +5,39 @@
 
 using namespace std;
 
+struct NodoAVL
+{
+    int id;
+    int puntaje;
+    string nombre;
+    NodoAVL *izq;
+    NodoAVL *der;
+    int fVal;
+
+    NodoAVL(int id, int puntaje, string nombre): id(id), puntaje(puntaje), nombre(nombre),izq(NULL), der(NULL), fVal(0) {}
+};
+
+struct RepresentacionAVL
+{
+  NodoAVL *raiz;
+  int cantElementos;
+  bool (*comparador)(NodoAVL*, NodoAVL*);
+
+  RepresentacionAVL(bool (*comparador) (NodoAVL*, NodoAVL*)): raiz(NULL), cantElementos(0), comparador(comparador){}
+};
+typedef RepresentacionAVL *AVL;
+
+
+
+bool compararId(NodoAVL* nuevo, NodoAVL* actual){
+    return nuevo->id > actual->id;
+}
+
+bool compararPuntaje(NodoAVL* nuevo, NodoAVL* actual){
+    if (nuevo->puntaje == actual->puntaje) return compararId (nuevo, actual);
+    else return nuevo->puntaje > actual->puntaje;
+}
+
 int main()
 {
     return 0;
