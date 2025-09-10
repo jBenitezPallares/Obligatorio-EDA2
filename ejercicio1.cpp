7dfe95edb1c3a0e784b6ff081ac67338b03e04a9
juanbenitez2005 <jbenitezpallares@gmail.com>
2025-09-09 23:54:43 -0300

    Agrege add

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -27,11 +27,119 @@ struct RepresentacionAVL
 };
 typedef RepresentacionAVL *AVL;
 
-NodoAVL* top1 (AVL a){
+// inserta el nodo en el AVL utilizando recursion
+bool insertarAVLRec(NodoAVL *&pa, int id, int puntaje, string nombre, bool &varioAltura, bool (*comparador)(NodoAVL*, NodoAVL*))
+{
+    if (pa == NULL)
+    {
+        pa = new NodoAVL(id, puntaje, nombre);
+        varioAltura = true;
+        return true;
+    }
+    if (id == pa->id) {
+        varioAltura = false;
+        return false;
+    }
+    NodoAVL nuevo(id, puntaje, nombre);
+    bool comparacion = comparador (&nuevo, pa);
+    bool insertado;
+    if (comparacion)
+    {
+        insertado = insertarAVLRec(pa -> izq, id, puntaje, nombre, varioAltura, comparador);
+        if (varioAltura)
+        {
+            switch (pa->fVal)
+            {
+                case 1:
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+                case 0:
+                    pa->fVal = -1;
+                    break;
+                case -1:
+                    NodoAVL *p1 = pa->izq;
+                    if(p1->fVal == -1){
+                        pa->izq = p1->der;
+                        p1->der = pa;
+                        pa->fVal = 0;
+                        pa = p1;
+                    } else {
+                        NodoAVL *p2 = p1->der;
+                        p1->der = p2->izq;
+                        p2->izq = p1;
+                        pa->izq = p2->der;
+                        p2->der = pa;
+                        pa->fVal = p2->fVal == -1 ? 1 : 0;
+                        p1->fVal = p2->fVal == 1 ? -1 : 0;
+                        pa = p2;
+                    }
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+            }
+            varioAltura = false;
+        }
+        return insertado;
+    }
+    else 
+    {
+        insertado = insertarAVLRec(pa->der, id, puntaje, nombre, varioAltura, comparador);
+        if (varioAltura)
+        {
+            switch (pa->fVal)
+            {
+                case -1:
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+                case 0:
+                    pa->fVal = 1;
+                    break;
+                case 1:
+                    NodoAVL *p1 = pa->der;
+                    if(p1->fVal == 1){
+                        pa->der = p1->izq;
+                        p1->izq = pa;
+                        pa->fVal = 0;
+                        pa = p1;
+                    } else {
+                        NodoAVL *p2 = p1->izq;
+                        p1->izq = p2->der;
+                        p2->der = p1;
+                        pa->der = p2->izq;
+                        p2->izq = pa;
+                        pa->fVal = p2->fVal == 1 ? -1 : 0;
+                        p1->fVal = p2->fVal == -1 ? 1 : 0;
+                        pa = p2;
+                        return insertado;
+                    }
+                    pa->fVal = 0;
+                    varioAltura = false;
+                    break;
+            }
+            varioAltura = false;
+        }
+        return insertado;
+    } 
+}
+
+// Metodo principal para insertar en el AVL
+void ADD(AVL a, int id, int puntaje, string nombre)
+{
+    bool varioAltura = false;
+    bool insertado = insertarAVLRec(a->raiz, id, puntaje, nombre, varioAltura, a->comparador);
+    if (insertado)
+    {
+        a->cantElementos++;
+    }
+}
+
+NodoAVL* TOP1 (AVL a){
     return a->raiz;
 }
 
-int count (AVL a){
+int COUNT (AVL a){
     return a->cantElementos;
 }
 
