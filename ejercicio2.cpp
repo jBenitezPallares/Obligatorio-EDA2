3a96374ff06e7ce52c06c60686ee0d2100cc52ef
juanbenitez2005 <jbenitezpallares@gmail.com>
2025-09-10 21:41:54 -0300

    Primera version de struct y primos

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -4,6 +4,64 @@
 #include <limits>
 
 using namespace std;
+// Tabla de hash cerrada para los dominios y dentro de cada nodo de la tabla, otra tabla de hash cerrada para los paths.
+
+bool esPrimo(int num){
+    if(num<=1 || num%2==0 && num!=2) return false;
+    if(num==2) return true;
+    for (int i = 3; i < num/2; i+=2)
+    {
+        if(num%i==0)
+        {
+            return false;
+        }
+    }
+    return true;    
+}
+
+int primoSupMinimo(int dato){
+    while(!esPrimo(++dato)); 
+    return dato;
+}
+
+struct nodoPath{
+    string path;
+    int tiempo;
+    bool estaBorrado;
+
+    nodoPath (string path, int tiempo): path(path), tiempo(tiempo), estaBorrado(false){}
+};
+
+struct nodoDominio {
+    string dominio;
+    int tope;
+    nodoPath** tablaPath;
+    int cantElementos;
+    //bool estaBorrado; preguntar profe si se borran doms
+
+    nodoDominio (int topeInicial, string d){
+        dominio = d;
+        cantElementos = 0;
+        tope = primoSupMinimo (topeInicial *2);
+        tablaPath = new nodoPath* [tope];
+        for (int i = 0; i < this -> tope; i++) tablaPath [i] = NULL;
+        //estaBorrado = false;
+    } 
+
+};
+
+struct representacionDominio{
+    int cantElementos;
+    int tope;
+    nodoDominio** tablaDoms;
+
+    representacionDominio (int topeInicial){
+        cantElementos = 0;
+        tope = primoSupMinimo (topeInicial * 2);
+        tablaDoms = new nodoDominio* [tope];
+        for (int i = 0; i < this -> tope; i++) tablaDoms [i] = NULL;
+    } 
+};
 
 int main()
 {
